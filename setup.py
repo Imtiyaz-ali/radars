9294c9800b0c22ace3f2264efc71611c12dfa6da
Imtiyaz-Ali <emteyazalee@gmail.com>
2024-03-04 19:59:50 +0530

    added git ignore

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 setup(
     name="radars",
-    version="0.0.1",
+    version="0.0.2",
     author = "emteyaz",
     author_email = "emteyazalee@gmail.com",
     description = "Trying to publish something",
@@ -13,5 +13,14 @@ setup(
     packages=find_packages(),
     install_requires = [
 
+    ],
+    url = "https://imtiyaz-ali.github.io/radars/",
+    project_urls = {
+        "Bug Tracker": "https://github.com/Imtiyaz-ali/radars/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
